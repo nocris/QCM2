eac5af4f26c88acab7f881e625bf3864e47803ca
cbrickley-matrice <cbrickley@matrice.io>
2022-12-01 23:50:12 +0100

    first

---
+++ QCM.js
@@ -0,0 +1,31 @@
+//Objet : deux manières de rajouter un attribut :
+
+
+
+//Objet : comment boucle-t-on sur un objet ? 
+
+
+
+//Objet : rajouter deux méthode de 2 manières différentes : 
+
+
+
+//Objet : à partir de l'objet suivant, renvoyez un tableau comportant uniquement les clés de l'objet :
+const obj = {
+    width : '10px',
+    height : '10px',
+    color : 'red'
+}
+
+
+
+//Class : écrivez une class (prenom, nom, age) et instanciez vous !
+
+
+
+//Ecrivez une sous-classe (possible de réutiliser la classe ci dessus)
+
+
+
+//Les 4 cas où la console renvoie undefined 
+
